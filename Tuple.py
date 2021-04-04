ff4c5bd63da9677cd35f580c5b4a63c36fd778a3
pratiksha sawandkar <63863917+pratikshas8080@users.noreply.github.com>
2021-04-04 19:00:53 +0530

    Tuple Intro and Use

---
+++ Tuple.py
@@ -0,0 +1,8 @@
+#Tuple
+Tuple1=("Patiksha", "Sudarshan", "Darsh", "Shree")
+var=Tuple1
+var=type(Tuple1)
+print(Tuple1)
+
+#Tuple are Unmutable that time you convert into List and Then add Elements
+
