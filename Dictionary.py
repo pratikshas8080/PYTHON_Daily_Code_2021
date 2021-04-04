076492bf110892652d85cb1bf48da1f28fe43786
pratiksha sawandkar <63863917+pratikshas8080@users.noreply.github.com>
2021-04-04 19:01:36 +0530

    Introduction How to use dict. in python

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+#Dict1
+Pratudict={
+    "Name" : "Pratiksha",
+    "Age" : "22 years Old",
+    "Fav Actor" : "Lin Yi",
+    "Relationship status":"Single",
+    "Trust Only" : "Ganpati Bappa_:)"
+} #Key Value Pairs
+print(Pratudict) #Print Dict 
+print(Pratudict["Fav Actor"]) #Print Only one Value in the dict
