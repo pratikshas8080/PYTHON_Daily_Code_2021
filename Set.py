b267c21daa134ff14d8bafc2ba617648e6f3acee
pratiksha sawandkar <63863917+pratikshas8080@users.noreply.github.com>
2021-04-04 19:00:23 +0530

    Introductio Of Set

---
+++ Set.py
@@ -0,0 +1,17 @@
+#set1
+s1={25,26,58,41,0,12,33,56}
+var=type(s1)
+print(var)
+
+s1.add(444444) #Add Number into Set
+print(s1)
+
+#set2
+s2={25,26,58,41,0,12,33,56}
+s2.update([85,23,33,6,9,3,555,69,74,22,10,33,22])
+print(s2)
+print(len(s2)) #Length Function
+print(s2)
+
+s2.remove(85)#Remove Which Element is Remove 
+print(s2)
