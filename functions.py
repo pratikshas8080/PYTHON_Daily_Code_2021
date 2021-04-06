e90c052ad6e63f142e0b0e39018e726b8618b0b6
pratiksha sawandkar <63863917+pratikshas8080@users.noreply.github.com>
2021-04-06 08:35:40 +0530

    Functions

---
+++ functions.py
@@ -0,0 +1,15 @@
+#functions
+def greet():
+    print("Gm Sir")
+
+greet()
+
+#function2
+a=int(input("Enter First Number"))
+b=int(input("Enter Second Number"))
+def sum(a, b):
+    c=a+b
+    return c
+
+d=sum(a,b)
+print(d)
