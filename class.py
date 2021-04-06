cf1408cc4b87cc9c38356a0200714606ea460090
pratiksha sawandkar <63863917+pratikshas8080@users.noreply.github.com>
2021-04-06 08:35:06 +0530

    class

---
+++ class.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, gname, gsalary):
+        self.name=gname
+        self.salary=gsalary
+
+Pratiksha=Employee("Pratiksha", 45000)
+print(Pratiksha.name)
+print(Pratiksha.salary)
